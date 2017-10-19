01971a99371eafe3b630d84f3147e5c69dddc8bb
tzachimmad <lapidotmmad@aim.com>
2017-10-19 16:25:26 +0300

    Add files via upload

---
+++ expense_class.py
@@ -0,0 +1,96 @@
+
+class Expense(object):
+    """Expense entry taken from Isracard output xls
+    """
+
+    def __init__(self, date_made, establishment, amount):
+        self.date_made = date_made
+        self.establishment = establishment
+        self.amount = amount
+
+    def get_amount(self,):
+        return self.amount
+
+    def get_establishment(self,):
+        return self.establishment
+
+    def get_date(self,):
+        return self.date_made
+
+    def get_month(self,):
+        month_year = self.date_made[self.date_made.find('/')+1:]
+        return int(month_year[:month_year.find('/')])
+
+    def get_year(self,):
+        month_year = self.date_made[self.date_made.find('/')+1:]
+        return int(month_year[month_year.find('/')+1:])
+
+    def get_category(self,):
+        return self.get_category
+
+class Establishment(object):
+    """Establishments in Isracard output xls
+    """
+    def __init__(self,name, category = None):
+        self.expenses = []
+        self.category = category
+        self.amount = 0
+        self.name = name
+        self.month_made = 0
+        self.year_made = 0
+
+    def get_expenses(self,):
+        return self.expenses
+
+    def get_category(self,):
+        return self.category
+
+    def add_expense(self, expense):
+        self.expenses.append(expense)
+        self.amount += int(expense.get_amount())
+        if self.month_made==0:
+            self.month_made = expense.get_month()
+            self.year_made = expense.get_year()
+
+    def set_category (self, categeory):
+        self.category = categeory
+
+    def get_name(self):
+        return self.name
+
+    def get_amount(self):
+        return self.amount
+
+    def set_amount(self, amount):
+        self.amount = amount
+
+    def get_month(self,):
+        return self.month_made
+
+    def get_year(self,):
+        return self.year_made
+
+class Category(object):
+    """Categories in Buisinesses csv
+    """
+
+    def __init__(self, name):
+        self.establishments = []
+        self.amount = 0
+        self.name = name
+
+    def get_amount(self,):
+        return self.amount
+
+    def get_establishments(self):
+        return self.establishments
+
+    def set_amount(self, amount):
+        self.amount = amount
+
+    def add_establishment(self, establishment):
+        self.establishments.append(establishment)
+        self.amount += int(establishment.get_amount())
+
+    def get_name(self):
+        return self.name
---
+++ expense_parser.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+from expense_class import *
+import xlrd
+from isracard_update import *
+from os import remove
+import os
+
+#define vars
+DOMESTIC_START = "עסקאות בארץ"
+CASH_ENTRY = "משיכת מזומנים"
+IGNORE_ISRACARD_ENTRY = "סך חיוב בש\"ח:"
+IGNORE_FRGN_ISRACARD_ENTRY = "TOTAL FOR DATE"
+CASH_ADVANCE_FEE = "CASH ADVANCE FEE"
+#dictionaries
+establishment_dic_array = [{},{},{},{},{},{},{},{},{},{},{},{}]
+
+def print_to_csv(output_file,year,month,key,value,new_line):
+    key = key.replace(",","")
+    key = key.replace("\'","")
+    key = key.replace("\"","")
+    output_file.write(str(year))
+    output_file.write(",")
+    output_file.write(str(month))
+    output_file.write(",")
+    output_file.write(key)
+    output_file.write(",")
+    output_file.write(str(value))
+    if new_line:
+        output_file.write("\n")
+
+#Create Expense and Relative Establishment and put in appropriate set
+def add_expense(date_made, estab_name, amount, frgn_expense):
+    month = int(date_made[date_made.find('/')+1:date_made.find('/')+3])
+    establishment_dic = establishment_dic_array[month-1]
+    if estab_name in establishment_dic:
+        estab = establishment_dic[estab_name]
+    else:
+        estab = Establishment(estab_name)
+        establishment_dic[estab_name] =  estab
+    expense_input = Expense(date_made, estab, amount)
+    estab.add_expense(expense_input)
+
+#parse isracard xls in html format
+def parse_xls_html(path, row_input,date_col,estab_col,amount_col,frgn_expense):
+    book = xlrd.open_workbook(filename=path, encoding_override="cp1252")
+    first_sheet = book.sheet_by_index(0)
+    row = row_input
+    keep_parsing = True
+    if first_sheet.cell(4,0).value!=DOMESTIC_START:
+        row = 7
+        estab_col = 2
+        amount_col = 5
+    while keep_parsing:
+        establishment = first_sheet.cell(row,estab_col).value
+        if establishment==IGNORE_FRGN_ISRACARD_ENTRY:
+            return -1
+        if establishment == IGNORE_ISRACARD_ENTRY:
+            row += 1
+            if first_sheet.cell(row,estab_col).value == xlrd.empty_cell.value:
+                keep_parsing = False
+            continue
+        if establishment == CASH_ADVANCE_FEE:
+            amount = first_sheet.cell(row,amount_col-2).value
+            date_made = first_sheet.cell(row-1,date_col).value
+        else:
+            amount = first_sheet.cell(row,amount_col).value
+            date_made = first_sheet.cell(row,date_col).value
+        add_expense(date_made, establishment, amount, frgn_expense)
+        row +=1
+    try:
+        if (first_sheet.cell(row+1,estab_col).value == xlrd.empty_cell.value):
+            return -1
+    except:
+        return -1
+    return row+2
+
+
+
+def output_data():
+    output_file = open("output.csv", 'w')
+    output_file.write("Year,Month,Establishment,Amount\n")
+    for month in range(0,12):
+        establishment_dic = establishment_dic_array[month]
+        pairs = sorted(establishment_dic.items(), key=lambda x: x[1].get_amount())
+        for tuple in reversed(pairs):
+            estab = tuple[1]
+            print_to_csv(output_file, estab.get_year(), estab.get_month(), estab.get_name(), estab.get_amount(), True)
+    output_file.close()
+
+folder_path = sys.argv[1]
+if (folder_path[-1]!='/'):
+    folder_path = folder_path+'/'
+print "Processing..."
+for fn in sorted(os.listdir(folder_path)):
+     if "Export" in fn and ".xls" in fn and fn[0]!='.':
+         print fn
+         next_row = parse_xls_html(folder_path + fn,6,0,1,2,False)
+         if next_row!=-1:
+             next_row = parse_xls_html(folder_path + fn,next_row,0,2,5,True)
+output_data()
+print "Done!"
